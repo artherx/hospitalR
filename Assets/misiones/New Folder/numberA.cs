fb503dbee58d46e5774c073d92135ce738d1df23
artherx <xxartherxx@gmail.com>
2024-11-05 15:46:35 -0500

    no se, probar

---
+++ Assets/misiones/New Folder/numberA.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class numberA : MonoBehaviour
+{
+    private coder co;
+    public int columna=0;
+    
+    public int number=0;
+    // Start is called before the first frame update
+    private void Start() {
+        co = GetComponentInParent<coder>();
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.tag == "dedo"){
+            switch(co.tipoPuzzle){
+                case coder.Puzzle.A:
+                    co.addNumber(number,co.tipoPuzzle);
+                    break;
+                case coder.Puzzle.B:
+                    
+                    co.addNumber(number,co.tipoPuzzle, columna);
+                    break;
+            }
+            
+        }
+    }
+}
