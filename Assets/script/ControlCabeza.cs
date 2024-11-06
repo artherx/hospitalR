8769fe4cf009420b6eb9f5d10d33683440c81044
artherx <xxartherxx@gmail.com>
2024-11-06 18:01:28 -0500

    mini cambios

---
+++ Assets/script/ControlCabeza.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlCabeza : MonoBehaviour
+{
+    private GameObject titere;
+    // Start is called before the first frame update
+    void Start()
+    {
+        titere = GameObject.Find("cabeza");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
